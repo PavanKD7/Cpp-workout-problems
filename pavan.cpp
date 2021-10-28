ecb6dc04b93eb26ecccd48a465bbd7c7f832636c
PavanKD7 <92989869+PavanKD7@users.noreply.github.com>
2021-10-28 18:37:55 +0530

    CPP Workout

---
+++ pavan.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <limits>
+using namespace std;
+int main() {
+	string name;
+	int num;
+	cout <<"The name is :" << name << flush;
+	cin >> name ;
+	cin >> num ;
+	cout <<"The number  is :" << num << flush;
+return 0;
+}
